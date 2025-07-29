fe7ccfafa9dcb3770333558b27599928d4999245
PinoqioDOM <alkhanishvilit@gmail.com>
2025-07-29 13:37:45 +0400

    First Step of E-commerce Web

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Cart from './pages/Cart'
+import Checkout from './pages/Checkout'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Products from './pages/Products'
+import SingleProduct from './pages/SingleProduct'
+import NotFound from './pages/NotFound'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} /> 
+        <Route path='/cart' element={<Cart />}/>
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/product-detail/:id" element={<SingleProduct />} />
+        <Route path="/notfound" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+
+export default App
