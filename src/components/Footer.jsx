42dc88b96cbc25e89edf875b7a399a60ae923d55
PinoqioDOM <alkhanishvilit@gmail.com>
2025-08-13 11:17:21 +0400

    Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import Logo from '../assets/Logo.png';
 
 const Footer = () => {
   return (
-    <div></div>
-  )
-}
+    <footer className="bg-[#90EE90] py-8 mt-8">
+      <div className="container mx-auto px-4 flex flex-col items-center justify-center text-center">
+        
+        <img
+          src={Logo}
+          alt="Veli Logo"
+          className="w-[150px] mb-4"
+        />
 
-export default Footer
+        <p className="text-xl font-semibold text-gray-800">
+          ადგილი სადაც მუდამ გელიან
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
